43c73650ebc315e53968a99f5ed9daef019d160b
KiyaTilahun <kiyatilahun0@gmail.com>
2024-03-15 00:21:07 +0300

    all done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  content: ["./**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
